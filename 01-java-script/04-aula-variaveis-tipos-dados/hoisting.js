53fa2ff15be8f0352f376fd517f8354617a47382
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-16 17:52:55 -0300

    feat: cria arquivo referente a hoisting

---
+++ 01-java-script/04-aula-variaveis-tipos-dados/hoisting.js
@@ -0,0 +1,8 @@
+// console.log('Minha atual é:', idade);
+
+var idade = 19;
+// variável global não pode ser referenciada antes da declaracao
+// var pode
+// let, const não podem ser referenciadas antes da inicializacao
+
+console.log('Minha idade depois de declarada é:', idade);
