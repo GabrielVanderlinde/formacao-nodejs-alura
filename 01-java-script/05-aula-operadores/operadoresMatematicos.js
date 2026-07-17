92c8513774a1a189bb68cb5df984cb903ee88a99
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 10:01:50 -0300

    feat: implementa arquivo destinado a operacoes matematicas

---
+++ 01-java-script/05-aula-operadores/operadoresMatematicos.js
@@ -0,0 +1,42 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+const soma = numeroUm + numeroDois;
+const subtracao = numeroUm - numeroDois;
+const multiplicacao = numeroUm * numeroDois;
+const divisao = numeroUm / numeroDois;
+
+console.log("Soma: " + soma);
+console.log("Subtração: " + subtracao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Divisão: " + divisao);
+
+const contaComplexa = (numeroUm + numeroDois) * numeroUm;
+console.log("Conta Complexa: " + contaComplexa);
+
+const restoDivisao = numeroDois % numeroUm;
+console.log("Resto da Divisão: " + restoDivisao);
+
+const potencia = 2 ** 3;
+console.log("Potência: " + potencia);
+
+let contador = 5;
+
+// contador = contador + 1;
+contador++;
+console.log("Contador após incremento: " + contador);
+
+// contador = contador - 1;
+contador--;
+console.log("Contador após decremento: " + contador);
+
+let numeroTres = 10;
+
+// numeroTres = numeroTres * 2; // numeroTres = numeroTres * 2;
+numeroTres *= 2; // numeroTres = numeroTres * 2;
+console.log("Número Três após multiplicação: " + numeroTres);
+
+// numeroTres += 5;
+// numeroTres -= 5;
+// numeroTres *= 5;
+// numeroTres /= 5;
