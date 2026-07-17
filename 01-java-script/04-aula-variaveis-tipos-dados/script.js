553edd64452c783a4777715c24fbb27e28887ca2
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 07:33:57 -0300

    refactor: renomeia arquivo

---
+++ 01-java-script/04-aula-variaveis-tipos-dados/script.js
@@ -0,0 +1,23 @@
+// Esse é um comentário do cópdigo! não será interpretado pelo JS
+
+// Formas diferentes de criar variáveis em JavaScript:
+// 1 - variável global
+// 2 - var - forma antiga de criar variáveis, não é mais recomendada
+// 3 - let  - forma moderna de criar variáveis, permite alterar o valor da variável
+// 4 - const - forma moderna de criar variáveis, não permite alterar o valor da variável
+
+// essa é uma variável que guarda minha idade atual
+let idade = 19;
+
+// essa é uma variável que guarda a data do meu aniversário
+const dataAniversario = "04/04/2007";
+
+// imprimindo no console a idade e a data do aniversário
+console.log("Minha idade é:", idade, "anos");
+console.log("Data do aniversário é:", dataAniversario);
+
+// minha idade depois do aniversário
+idade = 20;
+
+// imprimindo no console a idade depois do aniversário
+console.log("Minha idade depois do aniverário é:", idade, "anos");
