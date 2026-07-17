28343002dfc1af27fce41b8b91d995fb19db51da
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 07:35:35 -0300

    feat: cria arquivo referente a declaracao e tipos de variaveis

---
+++ 01-java-script/04-aula-variaveis-tipos-dados/tipos.js
@@ -0,0 +1,32 @@
+let nome = "Gabriel";
+let idade = 19;
+let altura = 1.85;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof altura);
+
+let estaChovendo = false;
+console.log(typeof estaChovendo); // false or true
+
+let cidade;
+
+console.log(typeof cidade); // undefined
+
+let aprendeuJS = null;
+
+console.log(typeof aprendeuJS); // null
+
+let simbolo = Symbol(); // Symbol é um tipo de dado primitivo que representa um valor único e imutável.
+// Ele é frequentemente usado como identificador para propriedades de objetos, garantindo que não haja colisões de nomes.
+
+console.log(typeof simbolo);
+
+// Possíveis tipos de dados em JavaScript:
+// string;
+//number;
+// number;
+// boolean;
+// undefined;
+// object;
+// symbol;
