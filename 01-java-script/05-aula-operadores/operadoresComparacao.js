9faa3584fb091fdafef643b3f53b55ad42dd6942
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 11:16:22 -0300

    feat: implementa arquivo destinado a operadores de Comparacao

---
+++ 01-java-script/05-aula-operadores/operadoresComparacao.js
@@ -0,0 +1,37 @@
+const idadeVinny = 36;
+const minhaIdade = 19;
+
+// operador igualdade
+// const comparacao = idadeVinny == minhaIdade
+//  const comparacao = idadeVinny != minhaIdade
+
+// maior que
+// const comparacao = idadeVinny > minhaIdade;
+
+// menor que
+// const comparacao = idadeVinny < minhaIdade;
+
+// Maior ou igual a
+//const comparacao = idadeVinny >= minhaIdade;
+
+// Menor ou igual a
+// const comparacao = idadeVinny <= minhaIdade;
+
+// console.log('São Diferentes? ' + comparacao);
+
+// --------------------------------------------------------------------------------------------------------------- //
+
+// const idade = 19
+// const idadeIrmao = 13
+
+// const souMaiorIdade = idade >= 18
+// const IrmaoMaiorIdade = idadeIrmao >= 18
+
+// console.log('Sou maior de idade? ' + souMaiorIdade);
+// console.log('Meu irmão é maior de idade? ' + IrmaoMaiorIdade);
+
+// --------------------------------------------------------------------------------------------------------------- //
+
+// Comparação estrita (compara valor e tipo de dado) ===
+const comparacaoEstrita = idadeVinny === minhaIdade;
+console.log("São estritamente iguais? " + comparacaoEstrita);
