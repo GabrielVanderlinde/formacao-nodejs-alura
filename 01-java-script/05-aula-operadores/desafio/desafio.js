40bca5100facf8853de5d186c4f593b7b64bc4c7
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 11:41:07 -0300

    feat: implementa desafio de operadores

---
+++ 01-java-script/05-aula-operadores/desafio/desafio.js
@@ -0,0 +1,11 @@
+console.log('Boas vindas a calculadora de trocos!');
+
+let valorCompra = 17.50;
+let valorPago = 20.00;
+
+console.log('O valor da compra é: ' + valorCompra);
+console.log('O valor pago é: ' + valorPago);
+
+let troco = valorPago - valorCompra;
+
+console.log('O valor do troco é: R$' + troco);
