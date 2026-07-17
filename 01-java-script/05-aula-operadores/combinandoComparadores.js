2373eb2c56a60b4a05ae2e3b7baff8af15d371ea
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 11:42:34 -0300

    feat: implementa arquivo js desntinado a logica de comparadores

---
+++ 01-java-script/05-aula-operadores/combinandoComparadores.js
@@ -0,0 +1,23 @@
+const idade = 17;
+const maiorDeIdade = idade >= 18;
+const possuiCNH = false;
+
+const podeDirigir = maiorDeIdade && possuiCNH; // AND
+console.log("Pode dirigir? " + podeDirigir);
+
+
+// ---------------------------------
+
+const podeViajarSozinha = maiorDeIdade || possuiCNH; // OR
+console.log("Pode viajar sozinha? " + podeViajarSozinha);
+
+// ---------------------------------
+
+const precisaAcompanhante = !maiorDeIdade; // NOT
+console.log("Precisa de acompanhante? " + precisaAcompanhante);
+
+// AND &&
+// OR ||
+// NOT !
+
+maiorIdade && possuiCNH || !precisaAcompanhante; // true && false = false
