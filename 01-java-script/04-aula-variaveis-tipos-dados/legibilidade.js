ee31f2fa6267e010f47efba4928bea87f73f5011
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 07:41:08 -0300

    feat: cria arquivo referente a lebilidade de variaveis

---
+++ 01-java-script/04-aula-variaveis-tipos-dados/legibilidade.js
@@ -0,0 +1,19 @@
+let alturaGabriel = 1.85;
+
+console.log("A altura do Gabriel é:", alturaGabriel, "metros");
+
+let altura = 7;
+let largura = 5;
+let area = altura * largura;
+
+console.log("A área do retângulo é:", area, "metros quadrados");
+
+// letras maiúsculas e minúsculas fazem diferença em JavaScript
+
+let preco = 10;
+let Preco = 20;
+
+console.log("O preço é:", preco);
+console.log("O Preço é:", Preco);
+console.log("O preço é:", preco, "e o Preço é:", Preco);
+// não funciona: console.log("O preço é:", precO);
