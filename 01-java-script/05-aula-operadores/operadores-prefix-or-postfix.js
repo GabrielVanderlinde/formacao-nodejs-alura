66f27b740d13c455435f8d5b8c73087e2ca79e56
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-17 10:05:48 -0300

    feat: implementa arquivo destinado a diferenca entre prefix ou postfix

---
+++ 01-java-script/05-aula-operadores/operadores-prefix-or-postfix.js
@@ -0,0 +1,9 @@
+// Funcionamento da Forma Prefixada
+
+let contador = 5;
+console.log(++contador); // imprime 6, pois a variável é incrementada antes da impressão
+
+// Funcionamento da Forma Posfixada
+
+let contadorDois = 5;
+console.log(contadorDois++); // imprime 5 e só depois incrementa; ao final, contador passa a valer 6
