09e2f6e034a0def551737a062c791666708efc8c
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-20 15:59:05 -0300

    feat: inicializar aula 09

---
+++ 01-java-script/09-arrays/arrays.js
@@ -0,0 +1,12 @@
+const frutas = ["Uva", "Banana", "Maça", "Kiwi"];
+
+console.log(frutas);
+console.log("Primeira fruta:", frutas[0]);
+
+console.log("Total de frutas:", frutas.length);
+
+frutas.push("Melancia");
+
+console.log("Total de frutas, depois da melancia:", frutas.length);
+
+console.log("Última fruta:", frutas[frutas.length - 1]);
