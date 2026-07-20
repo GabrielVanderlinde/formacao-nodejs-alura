b440deac12f1dfa9b397e0fea66a609165bf9845
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-20 16:38:36 -0300

    feat implementa logica de for, for/of e forEach

--- 01-java-script/09-arrays/arrays.js
+++ 01-java-script/09-arrays/arrays.js
@@ -1,12 +1,33 @@
 const frutas = ["Uva", "Banana", "Maça", "Kiwi"];
 
-console.log(frutas);
-console.log("Primeira fruta:", frutas[0]);
+// console.log(frutas);
+// console.log("Primeira fruta: ", frutas[0]);
 
-console.log("Total de frutas:", frutas.length);
+// console.log("Total de frutas:", frutas.length);
 
-frutas.push("Melancia");
+// frutas.push("Melancia");
 
-console.log("Total de frutas, depois da melancia:", frutas.length);
+// console.log("Total de frutas, depois da melancia:", frutas.length);
 
-console.log("Última fruta:", frutas[frutas.length - 1]);
+// console.log("Última fruta:", frutas[frutas.length - 1]);
+
+frutas.splice(2, 1);
+
+console.log("Depois de remover: ", frutas);
+
+for (let i = 0; i <= frutas.length; i++) {
+  console.log("indice: ", i);
+  console.log(frutas[i]);
+}
+
+console.log("Usando For Each:");
+
+frutas.forEach((valor, indice) => {
+  console.log("índice: ", indice, valor);
+});
+
+console.log("Usando for of: ");
+
+for (const fruta of frutas) {
+  console.log("fruta da vez: ", fruta);
+}
