633855f897e407269c6f6d2a5376fe89072c3120
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-21 11:58:08 -0300

    feat: inicializa aula 10

---
+++ 01-java-script/10-aula-objetos/objetos.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: "Ana",
+  idade: 26,
+  temCNH: true,
+};
+
+pessoa.sobrenome = 'Silva'
+
+console.log('Nome: ', pessoa.nome)
+console.log("Sobrenome: ", pessoa.sobrenome);
