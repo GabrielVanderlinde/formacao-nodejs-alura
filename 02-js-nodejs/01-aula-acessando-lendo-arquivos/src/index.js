5256cbccb1b4b5aead099bd6601466711596d63a
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-24 14:42:59 -0300

    refactor: refina a contagem

--- 02-js-nodejs/01-aula-acessando-lendo-arquivos/src/index.js
+++ 02-js-nodejs/01-aula-acessando-lendo-arquivos/src/index.js
@@ -20,13 +20,20 @@ function quebraEmParagrafos(texto) {
   console.log(contagem);
 }
 
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_~()]/g, "");
+}
+
 function verificaPalavrasDuplicadas(texto) {
   const listaPalavras = texto.split(" ");
   const resultado = {};
   //   objeto[propiedade] = valor;
 
   listaPalavras.forEach((palavra) => {
-    resultado[palavra] = (resultado[palavra] || 0) + 1;
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavra] = (resultado[palavraLimpa] || 0) + 1;
+    }
   });
 
   return resultado;
