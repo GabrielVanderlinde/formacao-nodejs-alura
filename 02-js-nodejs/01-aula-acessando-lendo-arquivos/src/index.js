3ff678947840bb265e64fb17b29611dc5474036f
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-24 11:50:59 -0300

    implementa logica de captura de palavras

--- 02-js-nodejs/01-aula-acessando-lendo-arquivos/src/index.js
+++ 02-js-nodejs/01-aula-acessando-lendo-arquivos/src/index.js
@@ -3,10 +3,22 @@ const fs = require("fs");
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    if (erro) {
-        console.error(erro);
-        return;
-    }
-    console.log(texto);
+fs.readFile(link, "utf-8", (erro, texto) => {
+  verificaPalavrasDuplicadas(texto);
 });
+
+// criar um array de palavras
+//contar as ocorrencias
+// montar um objeto com o resultado
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  //   objeto[propiedade] = valor;
+
+  listaPalavras.forEach((palavra) => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1;
+  });
+
+  console.log(resultado);
+}
