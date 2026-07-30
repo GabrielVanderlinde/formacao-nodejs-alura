dacae43e574a9c6140a499c3fdb1b18b073823c2
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-30 14:59:52 -0300

    feat: implementa logica de salvar arquivos

--- 02-js-nodejs/04-aula-import-export-promessas/src/cli.js
+++ 02-js-nodejs/04-aula-import-export-promessas/src/cli.js
@@ -4,12 +4,26 @@ import { contaPalavras } from "./index.js";
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
   try {
     if (erro) throw erro;
-    contaPalavras(texto);
+    const resultado = contaPalavras(texto);
+    criaSalvaArquivo(resultado, endereco);
   } catch (erro) {
     trataErros(erro);
   }
 });
+
+async function criaSalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = JSON.stringify(listaPalavras);
+
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("arquivo criado");
+  } catch (erro) {
+    throw erro;
+  }
+}
