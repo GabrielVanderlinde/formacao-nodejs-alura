f743c1156467f79d4e2a3c3f2b283f0e2c1f54c8
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-07-31 09:48:49 -0300

    feat: instala lib externa de commander

--- 02-js-nodejs/05-aula-npm-bibliotecas/src/cli.js
+++ 02-js-nodejs/05-aula-npm-bibliotecas/src/cli.js
@@ -1,29 +1,46 @@
-import fs from 'fs';
-import trataErros from './erros/funcoesErro.js';
-import { contaPalavras } from './index.js';
-import { montaSaidaArquivo } from './helpers.js';
+import fs from "fs";
+import trataErros from "./erros/funcoesErro.js";
+import { contaPalavras } from "./index.js";
+import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .option("-t,--texto <string>", "caminho")
+  .option("-d, --destino <string>", "caminho")
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error("erro: favor inserir o caminho de origem e destino");
+      program.help();
+      return;
+    }
+  });
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 const endereco = caminhoArquivo[3];
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
+fs.readFile(link, "utf-8", (erro, texto) => {
   try {
-    if (erro) throw erro
+    if (erro) throw erro;
     const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco)
-  } catch(erro) {
+    criaESalvaArquivo(resultado, endereco);
+  } catch (erro) {
     trataErros(erro);
   }
-})
+});
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
   const textoPalavras = montaSaidaArquivo(listaPalavras);
   try {
     await fs.promises.writeFile(arquivoNovo, textoPalavras);
-    console.log('arquivo criado');
-  } catch(erro) {
+    console.log("arquivo criado");
+  } catch (erro) {
     throw erro;
   }
 }
@@ -31,7 +48,7 @@ async function criaESalvaArquivo(listaPalavras, endereco) {
 // function criaESalvaArquivo(listaPalavras, endereco) {
 //   const arquivoNovo = `${endereco}/resultado.txt`;
 //   const textoPalavras = JSON.stringify(listaPalavras);
-  
+
 //   fs.promises.writeFile(arquivoNovo, textoPalavras)
 //     .then(() => {
 //       console.log('arquivo criado');
