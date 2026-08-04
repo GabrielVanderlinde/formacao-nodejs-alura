437146729fdb8b648f74d57cde52a0d0e65f3777
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-08-04 11:25:16 -0300

    feat: implementa crud de livros

--- 03-nodejs-api-rest-express-mongodb/src/app.js
+++ 03-nodejs-api-rest-express-mongodb/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const livros = [
   { id: 1, titulo: "Senhor dos Anéis" },
@@ -10,11 +11,37 @@ const livros = [
   },
 ];
 
+function buscarLivro(id) {
+  return livros.findIndex((livro) => livro.id === Number(id));
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js API REST com Express e MongoDB");
 });
 
 app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro adicionado com sucesso!");
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscarLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscarLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscarLivro(req.params.id);
+  livros.splice(index, 1);
   res.status(200).json(livros);
 });
 
