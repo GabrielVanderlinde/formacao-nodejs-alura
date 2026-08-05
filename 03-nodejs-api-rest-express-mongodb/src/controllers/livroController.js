8068d3c8ffc16352b3e56467f79610a29245bc02
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-08-05 15:03:53 -0300

    feat: finaliza crud de livros

--- 03-nodejs-api-rest-express-mongodb/src/controllers/livroController.js
+++ 03-nodejs-api-rest-express-mongodb/src/controllers/livroController.js
@@ -2,8 +2,26 @@ import livro from "../models/livro.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `Erro ao listar livros: ${erro.message}` });
+    }
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `Erro ao listar livro: ${erro.message}` });
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -12,6 +30,30 @@ class LivroController {
       res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
     } catch (erro) {
       res.status(500).json({ message: `Erro ao criar livro: ${erro.message}` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "atualizado com sucesso" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `Erro ao atualizar livro: ${erro.message}` });
+    }
+  }
+
+  static async deletarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: "livro excluído com sucesso" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `Erro ao excluir livro: ${erro.message}` });
     }
   }
 }
