cca4aa768e937109bd5a6da67d551a47e053d7ae
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-08-13 11:29:15 -0300

    feat: adiciona validações e mensagens de erro para os campos editora e numeroPaginas no modelo Livro

--- 04-nodejs-api-metricas/api-node-express/src/models/Livro.js
+++ 04-nodejs-api-metricas/api-node-express/src/models/Livro.js
@@ -8,8 +8,26 @@ const livroSchema = new mongoose.Schema({
     ref: "autores",
     required: [true, "O autor é obrigatório"],
   },
-  editora: { type: String, required: [true, "A editora é obrigatória"] },
-  numeroPaginas: { type: Number },
+  editora: {
+    type: String,
+    required: [true, "A editora é obrigatória"],
+    enum: {
+      values: ["Casa do Código", "Alura"],
+      message:
+        "A editora {VALUE} não é válida. As editoras válidas são: Casa do Código, Alura",
+    },
+  },
+  numeroPaginas: {
+    type: Number,
+    min: [
+      10,
+      "O número de páginas deve ser maior que zero. Valor fornecido: {VALUE}",
+    ],
+    max: [
+      5000,
+      "O número de páginas deve ser menor que 5.000. Valor fornecido: {VALUE}",
+    ],
+  },
 });
 
 const livros = mongoose.model("livros", livroSchema);
