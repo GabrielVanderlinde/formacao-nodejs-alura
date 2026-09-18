6a80061b807d043a8ac939e0094a2dc8c50cdc30
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-09-18 09:56:41 -0300

    feat: implementa exercicios da aula 02

---
+++ 00-javascript/01-javascript-estruturas-basicas/exercicios/5-var-let.js
@@ -0,0 +1,24 @@
+//Exercício voltado a Comparação de var e let
+
+var fora = "Fora do bloco";
+
+if (true) {
+  var dentro = "Dentro do bloco";
+  console.log(fora); // retorna 'Fora do bloco'
+  console.log(dentro); // retorna 'Dentro do bloco'
+}
+
+console.log(fora); // retorna 'Fora do bloco'
+console.log(dentro); // retorna 'Dentro do bloco'
+
+
+let fora = "Fora do bloco";
+
+if (true) {
+  let dentro = "Dentro do bloco";
+  console.log(fora); // retorna 'Fora do bloco'
+  console.log(dentro); // retorna 'Dentro do bloco'
+}
+
+console.log(fora); // retorna 'Fora do bloco'
+console.log(dentro); // erro 'dentro is not defined'
---
+++ 00-javascript/01-javascript-estruturas-basicas/exercicios/6-tempo.js
@@ -0,0 +1,7 @@
+const estaChovendo = false;
+
+if (estaChovendo) {
+  console.log("Está chovendo! Se prepare, com um guarda-chuva!");
+} else {
+  console.log("Não está chovendo! Não precisa de guarda-chuva");
+}
