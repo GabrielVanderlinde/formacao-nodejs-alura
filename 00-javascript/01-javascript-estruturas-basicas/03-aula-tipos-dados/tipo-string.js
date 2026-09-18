0da66f8689910c94ee7e8dcf59f9fca4e253f287
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-09-18 15:37:34 -0300

    feat: implementa conteudos base da aula

---
+++ 00-javascript/01-javascript-estruturas-basicas/03-aula-tipos-dados/tipo-string.js
@@ -0,0 +1,9 @@
+const estudante = 'Gabriel';
+const docente = 'Ana';
+const cumprimento = "nosso lema é 'estudar bastante!'";
+const citacao = `Ju diz: "Nosso lema é 'estudar bastante!'"`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`a estudante chama ${estudante}`);
